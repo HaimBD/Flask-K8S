8be0f1eb4ea321496ee3b673edbe9bb3d7c8d562
HaimBD <haimbendavid1995@gmail.com>
2025-05-01 18:06:23 +0000

    Trying to trigger pipeline

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "This is the original message. The next step is to change it via CI/CD"
+    return "This is the original message. The next step is to change it via CI/CD..."
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
